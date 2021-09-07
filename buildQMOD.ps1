074989bf2553c5bc698357654b2212ad0ac37d3f
OptimusChen <derek.hz.chen@gmail.com>
2021-09-07 11:48:09 -0700

    changed version

--- buildQMOD.ps1
+++ buildQMOD.ps1
@@ -6,8 +6,8 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
     $buildScript += ".cmd"
 }
 
-$ArchiveName = "SongInfoPlus_v0.1.0.qmod"
-$TempArchiveName = "SongInfoPlus_v0.1.0.qmod.zip"
+$ArchiveName = "SongInfoPlus_v1.0.0.qmod"
+$TempArchiveName = "SongInfoPlus_v1.0.0.qmod.zip"
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
 Compress-Archive -Path "./libs/arm64-v8a/libSongInfoPlus.so", "./libs/arm64-v8a/libbeatsaber-hook_2_3_0.so", "logo.png", ".\arrow.png", ".\arrow_down.png", "./mod.json" -DestinationPath $TempArchiveName -Force
